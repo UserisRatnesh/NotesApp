0f06b53dce409077478b5807f87769f6cce0b873
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-07-24 01:46:56 +0530

    Limited the number of characters being shown under each note.

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -7,20 +7,48 @@ import mongoose from "mongoose";
 
 export async function dashboard(req, res) {
 
+    let perPage = 12;
+    let page = req.query.page || 1;
+
     const locals = {
         title: "Dashboard",
         description: 'A Free note taking App'
     }
 
     try {
-        const notes = await Note.find({});
-        console.log(notes);
+
+        const notes = await Note.aggregate([
+            {
+                $sort: {
+                    createdAt: -1
+                }
+            },
+            {
+                $match: { user: new mongoose.Types.ObjectId(req.user.id) }
+            },
+            {
+                $project: {
+                    title: { $substr: ["$title", 0, 30] },
+                    body: { $substr: ["$body", 0, 100] },
+                },
+            }
+        ])
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+
+        const count = await Note.countDocuments({ user: new mongoose.Types.ObjectId(req.user.id) });
         res.render("dashboard/index.ejs", {
             userName: req.user.firstName,
             locals,
             notes,
-            layout: "../views/layouts/dashboard"
+            layout: "../views/layouts/dashboard",
+            current: page,
+            pages: Math.ceil(count / perPage)
         });
+
+
+
+
     } catch (error) {
         console.log(error);
     }
